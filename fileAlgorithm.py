9e6b523ca16f295c87da8fd68d4fec67ae8c0c8d
ARNisUsername <67711082+ARNisUsername@users.noreply.github.com>
2020-08-30 11:21:19 -0700

    Update fileAlgorithm.py

--- fileAlgorithm.py
+++ fileAlgorithm.py
@@ -6,22 +6,22 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42)
 
 lr = LinearRegression().fit(X_train, y_train)
 
-'''Ridge Regression(uses regularization: each feature should have as little affect on the outcome as possible while
-being able to predict correctly to avoid overfitting) - increase alpha means increase in generalization, decrease in training accuracy'''
+#Ridge Regression(uses regularization: each feature should have as little affect on the outcome as possible while
+#being able to predict correctly to avoid overfitting) - increase alpha means increase in generalization, decrease in training accuracy
 
 from sklearn.linear_model import Ridge
 
 ridge = Ridge(alpha=1.0).fit(X_train, y_train)
 
-'''Lasso(Alternate to Ridge, but some feautures become completely ignored to make the model easier to intepret; L1 Regularization)
-An increase in alpha would be an increase in amount of feautures used'''
+#Lasso(Alternate to Ridge, but some feautures become completely ignored to make the model easier to intepret; L1 Regularization)
+#An increase in alpha would be an increase in amount of feautures used
 
 from sklearn.linear_model import Lasso
 
 lasso = Lasso(alpha=0.01, max_iter=100000).fit(X_train, y_train)
 
-'''Logistic Regression(One type of linear model used for classification; L2 Regularization by default)
-The higher C is, the increased complexity of the model(which makes the predictory better in most cases)'''
+#Logistic Regression(One type of linear model used for classification; L2 Regularization by default)
+#The higher C is, the increased complexity of the model(which makes the predictory better in most cases)
 
 logreg = LogisticRegression(C=1,max_iter=10000,penalty="l2").fit(X_train, y_train)
 
