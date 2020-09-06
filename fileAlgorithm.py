2c827cb1db06ef033274da500a95fdc3bc5c36b9
ARNisUsername <67711082+ARNisUsername@users.noreply.github.com>
2020-09-06 10:47:54 -0700

    Update fileAlgorithm.py

--- fileAlgorithm.py
+++ fileAlgorithm.py
@@ -45,3 +45,20 @@ forest = RandomForestClassifier(n_estimators=10,random_state=0).fit(X_train, y_t
 from sklearn.ensemble import GradientBoostingClassifier
 
 gbrt = GradientBoostingClassifier(random_state=0,max_depth=3,learning_rate=0.1).fit(X_train, y_train)
+
+#SVMs: A type of linear model which uses the kernel trick to transform your data and find the best boundary between all the possible feautures of outputs(POWERFUL!!!)
+#Kernel trick: 'poly'/Polynomial Kernel: Computes all possible polynomials up to a certain degree of the original feautures(e.g. feauture1 ** 2 * feauture2 ** 5)
+#Kernel trick: 'rbf'/Gaussian Kernel: It consideres all possible polynomials for all degrees, but the importance of each feauture decreases with a higher degree
+#gamma: low gamma means lower complexity of model, high gamma means higher complexity of model
+#C: low C means each data point has a limited influence on model, high C means each datapoint has higher influence on model(use higher C when model underfitting)
+
+from sklearn.svm import SVC
+from sklearn.preprocessing import StandardScaler
+
+#scale data to 0 and 1
+min_on_training = X_train.min(axis=0)
+range_on_training = (X_train - min_on_training).max(axis=0)
+X_train_scaled = (X_train - min_on_training) / range_on_training
+X_test_scaled = (X_test - min_on_training) / range_on_training)
+#calculate svm
+svm = SVC(kernel='rbf',C=10,gamma=0.1).fit(X_train_scaled, y_train)
