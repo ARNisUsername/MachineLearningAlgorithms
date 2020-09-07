d5db30baa5c6ce54a691291ac8b7bf9fad03d329
ARNisUsername <67711082+ARNisUsername@users.noreply.github.com>
2020-09-06 17:16:12 -0700

    Update fileAlgorithm.py

--- fileAlgorithm.py
+++ fileAlgorithm.py
@@ -1,13 +1,17 @@
+
+#Make mglearn wave(Not Machine Learning, just useful for future reference
+X, y = mglearn.datasets.make_wave(n_samples=60)
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42)
+
 #KNeighborsClassifier:
 
 from sklearn.neighbors import KNeighborsClassifier
+
 knn = KNeighborsClassifier(n_neighbors=3).fit(X_train, y_train)
 
 #Linear Regression:
 
 from sklearn.linear_model import LinearRegression
-X, y = mglearn.datasets.make_wave(n_samples=60)
-X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42)
 
 lr = LinearRegression().fit(X_train, y_train)
 
