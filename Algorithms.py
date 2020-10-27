560a2efefb760453e1365aa69e0576381b1025b7
Arnav Sharma <67711082+ARNisUsername@users.noreply.github.com>
2020-10-26 18:39:26 -0700

    Update Algorithms.py

--- Algorithms.py
+++ Algorithms.py
@@ -105,9 +105,7 @@ plt.show()
 #MinMaxScaler(scales data, very common for neural networks
 from sklearn.preprocessing import MinMaxScaler
 scaler = MinMaxScaler()
-scaler.fit(X_train)
-X_train = scaler.transform(X_train)
-scaler.fit(X_test)
+X_train = scaler.fit_transform(X_train)
 X_test = scaler.transform(X_test)
 
 #Tensorflow neural network
