58227565e128ebad9f0737b20c3a9af2fd265c1f
Arnav Sharma <67711082+ARNisUsername@users.noreply.github.com>
2020-12-10 17:24:42 -0800

    Update KNeighbors.py

--- manualAlgorithms/KNeighbors.py
+++ manualAlgorithms/KNeighbors.py
@@ -1,84 +1,39 @@
 import numpy as np
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
 
-def convertDif(array, way='str'):
-    for i in range(len(array)):
-        if way == 'str':
-            array[i] = str(array[i])
-        elif way == 'int':
-            array[i] = int(array[i])
-    return array
+def distance_vector(vector1, vector2):
+    distance_n = []
+    for n in range(len(vector1)):
+        distance_n.append((vector1[n]-vector2[n])**2)
+    return np.sqrt(sum(distance_n))
 
-def orderKeys(theDict):
-    newDict = {}
-    theKeys = np.sort(list(theDict.keys()))
-    for key in theKeys:
-        newDict[key] = theDict[key]
-    return newDict
-
-def countInstances(theList):
-    theIn = list(np.unique(theList))
-    theDict = {}
-    for i in theIn:
-        theDict[i] = 0
-    for i in theList:
-        theDict[i] += 1
-    return theDict
-
-def output_dict(x_array, y_array):
-    info = {}
-    for i in range(len(x_array)):
-        newKey = ' '.join(convertDif(x_array[i]))
-        info[newKey] = y_array[i]
-    return info
-
-def closest(theDict,point,k):
-    info = {}
-    
-    for i in range(len(theDict.keys())):
-        pointList = convertDif(list(list(theDict.keys())[i].split()),'int')
-        xDif = np.absolute(point[0] - pointList[0])
-        yDif = np.absolute(point[1] - pointList[1])
-        distance = np.sqrt(xDif**2+yDif**2)
-
-        pointClass = theDict[list(theDict.keys())[i]]
-        if distance in list(info.keys()):
-            distance += np.random.rand()/10000
-        info[distance] = pointClass
-
-    orderedDict = orderKeys(info)
-    orderedClasses = list(orderedDict.values())
-
-    match = countInstances(orderedClasses[:k])
-    listMatches = list(match.values())
-    if listMatches.count(listMatches[0]) == len(listMatches) and len(listMatches) > 1:
-        match = countInstances(orderedClasses[:k-1])
-
-    theKey = ''
-    currentMax = 0
-    for key in match.keys():
-        if match[key] > currentMax:
-            currentMax = match[key]
-            theKey = key
-            
-    return theKey
-    
 class ManualKNeighbors:
-    def __init__(self, k, xarray=None, yarray=None):
-        self.k = k
-        self.xarray = xarray
-        self.yarray = yarray
+    def __init__(self, x=None, y=None):
+        self.x = x
+        self.y = y
         
-    def fit(self, x_array, y_array):
-        self.xarray = x_array
-        self.yarray = y_array
+    def fit(self, the_x, the_y):
+        self.x = the_x
+        self.y = the_y
 
-    def predict(self, newPoint):
-        return closest(output_dict(self.xarray,self.yarray), newPoint, self.k)
-    
+    def predict(self, x_pred):
+        all_distances = [distance_vector(x_pred, self.x[i]) for i in range(len(self.x))]
+        return self.y[np.argmin(all_distances)]
 
-X = [[2,5],[2,6],[3,5],[3,6],[4,3],[4,4],[5,3],[5,4],[6,1],[6,2],[7,1],[7,2]]
-y = [1,1,1,1,0,0,0,0,2,2,2,2]
+    def predict_multiple(self, x_preds):
+        return [self.predict(x_vector) for x_vector in x_preds]
 
-knn = ManualKNeighbors(k=3)
-knn.fit(X, y)
-print(knn.predict([1,7])) #Predicts 1, like how it should
+    def accuracy(self, x_test, y_test):
+        predictions = [self.predict(test_vector) for test_vector in x_test]
+        amount_correct = [predictions[i] == y_test[i] for i in range(len(predictions))].count(True)
+        return amount_correct/len(predictions)
+
+data = load_iris()
+X = data.data
+y = data.target
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
+
+model = ManualKNeighbors()
+model.fit(X_train, y_train)
+print(model.accuracy(X_test, y_test))
